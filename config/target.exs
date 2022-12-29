6fd8da86e4e9e1ed00d0b159ccb5f54d8212debd
Powell Kinney <628952+pkinney@users.noreply.github.com>
2022-12-29 11:53:10 -0600

    Remove old comment

--- config/target.exs
+++ config/target.exs
@@ -104,10 +104,6 @@ config :mdns_lite,
     }
   ]
 
-################################################################
-## Phoenix config for target device
-################################################################
-
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
